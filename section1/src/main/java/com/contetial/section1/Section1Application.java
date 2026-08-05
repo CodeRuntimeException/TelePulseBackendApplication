4d23cd1b681bfd44294a0fc1de0faa03ece5fb19
pranay <pranay@gmail.com>
2026-08-05 04:02:07 -0700

    Mahak Dev Getting Started

--- section1/src/main/java/com/contetial/section1/Section1Application.java
+++ section1/src/main/java/com/contetial/section1/Section1Application.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Section1Application {
+public class Section1Application
+{
+    //Mahak Lalwani Contetial
 
 	public static void main(String[] args) {
 		SpringApplication.run(Section1Application.class, args);
